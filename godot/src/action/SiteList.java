1932f6cd840576998a7405503af595fa1baf2655
huoxiaoduo <huoxiaoduo@dratio.com>
2015-09-15 15:24:54 +0800

    增加有输出的用户数量和mcookie大于1000的用户数量

--- godot/src/action/SiteList.java
+++ godot/src/action/SiteList.java
@@ -17,6 +17,8 @@ import data.Site;
 public class SiteList extends ActionSupport {
 	
 	List<Site> list = new ArrayList<Site>();
+	String ftpCount = "0"; // 输出的用户数量：
+	String mcookieCount = "0"; // mcookie 》 1000
 	public String execute(){
 		HttpServletRequest request = ServletActionContext.getRequest();
 		HttpServletResponse response = ServletActionContext.getResponse();
@@ -24,6 +26,18 @@ public class SiteList extends ActionSupport {
 		if(session.getAttribute("user").equals("admin2015")){
 			SiteDao siteDao = new SiteDao();
 			list = siteDao.getsites();
+			int fcount = 0 ;
+			int mcount = 0 ;
+			for (Site site : list) {
+				if(site.getFtp().equals("1")){
+					fcount++;
+				}
+				if(site.getMcookie()>1000){
+					mcount++;
+				}
+			}
+			ftpCount = String.valueOf(fcount);
+			mcookieCount = String.valueOf(mcount);
 			return SUCCESS;
 		}
 		return "home";
@@ -37,4 +51,20 @@ public class SiteList extends ActionSupport {
 		this.list = list;
 	}
 
+	public String getFtpCount() {
+		return ftpCount;
+	}
+
+	public void setFtpCount(String ftpCount) {
+		this.ftpCount = ftpCount;
+	}
+
+	public String getMcookieCount() {
+		return mcookieCount;
+	}
+
+	public void setMcookieCount(String mcookieCount) {
+		this.mcookieCount = mcookieCount;
+	}
+	
 }
